5ab4236a08f02fffc0e0dca748e38f03a4bf72f7
gotticketsto360tour <werling1407@gmail.com>
2020-12-08 12:47:16 +0100

    Test multiprocessing on other computer

--- SmartHome/preprocessing/clean_comment.py
+++ SmartHome/preprocessing/clean_comment.py
@@ -290,9 +290,73 @@ subset = df.head(10)
 import multiprocessing as mp
 
 pool = mp.Pool(2)
-results = pool.map(clean_comment, subset["text"].values)
+results = pool.map(clean_comment, subset["text"].values, chunksize=3)
 #results = [pool.apply(clean_comment, args = row) for row in subset["text"].values]
 pool.close()
 print(results)
 
+
+from multiprocessing import Pool
+def f(x):
+    time.sleep(2) # Wait 2 seconds
+    print(x*x)
+p = Pool(8)
+p.map(f, [1, 2, 3, 4])
+p.close()
+p.join()
+
+
 ## EXPERIMENTING WITH THE SPECIFIC SYNTAX:
+
+def main():
+    values = subset["text"].values
+
+    with mp.get_context("spawn").Pool(processes = 5) as pool:
+        print("is anything happening?")
+        data = pool.map(clean_comment, subset["text"].values)
+    
+    return data
+        
+main()
+#clean_comment(subset, "text")
+
+
+import numpy as np
+from time import time
+
+# Prepare data
+np.random.RandomState(100)
+arr = np.random.randint(0, 10, size=[200000, 5])
+data = arr.tolist()
+data[:5]
+
+# Solution Without Paralleization
+
+def howmany_within_range(row, minimum, maximum):
+    """Returns how many numbers lie within `maximum` and `minimum` in a given `row`"""
+    count = 0
+    for n in row:
+        if minimum <= n <= maximum:
+            count = count + 1
+    return count
+
+results = []
+for row in data:
+    results.append(howmany_within_range(row, minimum=4, maximum=8))
+
+print(results[:10])
+
+# Parallelizing using Pool.apply()
+
+import multiprocessing as mp
+
+# Step 1: Init multiprocessing.Pool()
+pool = mp.Pool(mp.cpu_count())
+
+# Step 2: `pool.apply` the `howmany_within_range()`
+results = [pool.apply(howmany_within_range, args=(row, 4, 8)) for row in data]
+
+# Step 3: Don't forget to close
+pool.close()    
+
+print(results[:10])
